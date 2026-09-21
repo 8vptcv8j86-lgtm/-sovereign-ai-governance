39041254ebd2ae93d796f1919ca7bb10258799cf
8vptcv8j86-lgtm <8vptcv8j86@privaterelay.appleid.com>
2026-09-21 07:25:16 -0500

    Make Cloudflare D1 declaration lint safe

--- cloudflare-runtime.d.ts
+++ cloudflare-runtime.d.ts
@@ -2,7 +2,9 @@ interface Fetcher {
   fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
 }
 
-interface D1Database {}
+interface D1Database {
+  readonly __sentinelD1DatabaseBrand?: never;
+}
 
 declare module "cloudflare:workers" {
   export const env: Record<string, unknown>;
